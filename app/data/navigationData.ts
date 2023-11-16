4c854ff46f19145dab61f457a048219198c701a8
Hossam Cj <hossamcj@gmail.com>
2023-11-17 00:44:37 +0200

    Initial commit

---
+++ app/data/navigationData.ts
@@ -0,0 +1,22 @@
+export const navMenuList = [
+    {
+        title: "Home",
+        href: "#home",
+    },
+    {
+        title: "Projects",
+        href: "#projects",
+    },
+    {
+        title: "About",
+        href: "#about",
+    },
+    {
+        title: "Skills",
+        href: "#skills",
+    },
+    {
+        title: "Hire me",
+        href: "mailto:hossamcj@gmail.com",
+    },
+];
